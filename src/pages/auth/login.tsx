e54a9daaf21c5ed5e5346660c1a722b1cc24275c
zemikyafu <zemikfcb@gmail.com>
2025-01-05 23:38:33 +0200

    add auth api impl

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,10 +1,36 @@
-import React from "react"
+import React, { useState } from "react"
 import { Button } from "../../components/ui/button"
+import { login, register } from "../../api/AuthService"
+import { useNavigate } from "react-router-dom"
 export function Login() {
+  const [formState, setFormState] = useState({ email: "", password: "" })
+
+  const navigate = useNavigate()
+  const [error, setError] = useState("")
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
+    try {
+      const response = await login(formState.email, formState.password)
+      localStorage.setItem("token", response.token)
+      localStorage.setItem("user", response.name)
+      localStorage.setItem("userId", response.userId)
+      navigate("/home")
+    } catch (err) {
+      setError((err as Error).message)
+    }
+  }
+
+  const handleOnChaneg = (e: React.FormEvent) => {
+    const { name, value } = e.target as HTMLInputElement
+    setFormState({ ...formState, [name]: value })
+  }
+
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="w-full max-w-md p-8 space-y-8 bg-white rounded shadow-md">
         <h2 className="text-2xl font-bold text-center">Login</h2>
+        {error && <p className="text-red-500">{error}</p>}
         <form className="mt-8 space-y-6">
           <div className="rounded-md shadow-sm">
             <div>
@@ -17,6 +43,7 @@ export function Login() {
                 type="email"
                 autoComplete="email"
                 required
+                onChange={handleOnChaneg}
                 className="relative block w-full px-3 py-2 border border-gray-300 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 placeholder="Email address"
               />
@@ -31,6 +58,7 @@ export function Login() {
                 type="password"
                 autoComplete="current-password"
                 required
+                onChange={handleOnChaneg}
                 className="relative block w-full px-3 py-2 border border-gray-300 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 placeholder="Password"
               />
@@ -57,6 +85,7 @@ export function Login() {
           <div>
             <Button
               type="submit"
+              onClick={handleSubmit}
               className="relative flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md group hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
               Sign in
@@ -67,5 +96,3 @@ export function Login() {
     </div>
   )
 }
-
-// export default Login;
