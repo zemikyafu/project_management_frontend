a549e5e21527d50e9c554d27b30926c701cc35e9
zemikyafu <zemikfcb@gmail.com>
2025-01-05 23:33:57 +0200

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import { Home } from "./pages/home"
-
+import { Login } from "./pages/auth/login"
+import { Register } from "./pages/auth/register"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import PrivateRoute from "./components/privateRoute"
+import Profile from "./pages/auth/profile"
 function App() {
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/home" element={<PrivateRoute element={<Home />} />} />
+          <Route path="/profile" element={<PrivateRoute element={<Profile />} />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
