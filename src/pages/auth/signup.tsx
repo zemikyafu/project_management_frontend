676f6365061d6a1b644e9710fcf76e1c4e0cb5db
zemikyafu <zemikfcb@gmail.com>
2025-01-08 16:10:01 +0200

    update signup page

---
+++ src/pages/auth/signup.tsx
@@ -0,0 +1,10 @@
+import SignupForm from "../../components/signup-form"
+export function SignupPage() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-3xl">
+        <SignupForm />
+      </div>
+    </div>
+  )
+}
