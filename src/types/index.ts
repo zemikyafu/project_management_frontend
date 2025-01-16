6549abdec7277ca125103ae12a37fcdd1fad8553
zemikyafu <zemikfcb@gmail.com>
2025-01-16 12:23:38 +0200

    add project related types

--- src/types/index.ts
+++ src/types/index.ts
@@ -11,3 +11,19 @@ export interface Company{
     name: string;
     address: string;
 }
+export enum ProjectStatus {
+    NOT_STARTED = "NOT_STARTED",
+    IN_PROGRESS = "IN_PROGRESS",
+    COMPLETED = "COMPLETED",
+    ON_HOLD = "ON_HOLD"
+}
+
+export interface Project {
+    id: string;
+    name: string;
+    description: string;
+    status: ProjectStatus;
+    workspaceId: string;
+    startDate: string;
+    endDate: string;
+}
